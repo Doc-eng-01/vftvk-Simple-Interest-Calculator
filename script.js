4a9f238410e855876dfbf24c4eeb8c3eb01e8f53
Satendra Bisht <sbisht@mycom-internal.com>
2021-10-04 18:03:38 +0530

    Updated files

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ function compute()
     p = document.getElementById("principal").value;
     /*checking principal value for negative and zero */
     if (p <= 0 || p == ""){
-      alert("Enter a correct principal value")
+      alert("Enter a positive number");
+      document.getElementById("principal").focus;
     }
+    
     q = document.getElementById("rate").value;
     console.log("Rate is:"+q);
     r = document.getElementById("years").value;
@@ -15,25 +17,28 @@ function compute()
     yr=new Date().getFullYear();
     maturityYear = (yr+intRate); 
     console.log(maturityYear);
-    const para = document.createElement("span");
-    var node = document.createTextNode("If you deposit " +p+",");
-    para.appendChild(node);
-    var linebreak = document.createElement('br');
-    para.appendChild(linebreak);
-    var node = document.createTextNode("at an interest rate of "+intRate+".");
-    para.appendChild(node);
-    var linebreak = document.createElement('br');
-    para.appendChild(linebreak);
-    var node = document.createTextNode("You will receive an amount of "+a+",");
-    para.appendChild(node);
-    var linebreak = document.createElement('br');
-    para.appendChild(linebreak);
-    var node = document.createTextNode("in the year "+maturityYear);
-    para.appendChild(node);
-    var linebreak = document.createElement('br');
-    para.appendChild(linebreak);
-    const element = document.getElementById("result");
-    element.appendChild(para);
+
+    /* adding variables to output */
+    document.getElementById("result").innerHTML="If you deposit "+p+",\<br\>at an interest rate of "+intRate+"\<br\>You will receive an amount of "+a+"\<br\>in the year "+maturityYear+"\<br\>";
+    // const para = document.createElement("span");
+    // var node = document.createTextNode("If you deposit " +p+",");
+    // para.appendChild(node);
+    // var linebreak = document.createElement('br');
+    // para.appendChild(linebreak);
+    // var node = document.createTextNode("at an interest rate of "+intRate+".");
+    // para.appendChild(node);
+    // var linebreak = document.createElement('br');
+    // para.appendChild(linebreak);
+    // var node = document.createTextNode("You will receive an amount of "+a+",");
+    // para.appendChild(node);
+    // var linebreak = document.createElement('br');
+    // para.appendChild(linebreak);
+    // var node = document.createTextNode("in the year "+maturityYear);
+    // para.appendChild(node);
+    // var linebreak = document.createElement('br');
+    // para.appendChild(linebreak);
+    // const element = document.getElementById("result");
+    // element.appendChild(para);
  
 }
 
